82cb9e55c75b9420d74ca2476f439ca815467455
GustavoMazzaro <147075000+GustavoMazzaro@users.noreply.github.com>
2024-12-26 09:43:28 -0300

    Cadastro

---
+++ Cadastro.py
@@ -0,0 +1,14 @@
+nome = input('digite seu nome: ')
+sobrenome = input('digite seu sobrenome: ')
+data = int(input('digite sua idade: '))
+ano_nascimento = int(input('digite o ano de nascimento: '))
+maior_idade = data > 18
+altura = float(input('digite sua altura: '))
+
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', data)
+print('Ano de Nascimento: ', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros: ', altura)
