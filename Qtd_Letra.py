97bc10bc91ac1e915d0e65f85878dcd51efcce5d
GustavoMazzaro <147075000+GustavoMazzaro@users.noreply.github.com>
2025-01-09 21:55:10 -0300

    Quantidade_Letra

---
+++ Qtd_Letra.py
@@ -0,0 +1,28 @@
+frase = 'Mas os que esperam no Senhor renovarão as suas forças e subirão com asas como águias;' \
+        'correrão e não se cansarão;' \
+        'caminharão e não se fatigarão.'
+
+i =0 
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_atual = frase.count(letra_atual)
+    
+    if qtd_apareceu_mais_vezes < qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_apareceu_mais_vezes}" que apareceu '
+    f'{qtd_apareceu_mais_vezes}x'
+)
